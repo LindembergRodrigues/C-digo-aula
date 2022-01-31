eaea8688096446cc62d182982b3cee7440fafb5d
Lindemberg Barbosa <lindemberg.barbosa@ccc.ufcg.edu.br>
2022-01-31 01:50:50 -0300

    atualização

--- Algoritmos de ordenação/OrdenacaoInsertion.java
+++ Algoritmos de ordenação/OrdenacaoInsertion.java
@@ -1,3 +1,24 @@
-public class OrdenacaoInsertion{
-    
+public class OrdenacaoInsertion {
+    public static void insertion(int [] lista){
+        
+        for (int i=1; i< lista.length;i++){
+            int chave = lista[i];
+            for (int j =i; j>0;j--){
+                if (chave < lista[j-1]){
+                    int aux = lista[j-1];
+                    lista[j-1] = chave;
+                    lista[j] = aux;
+                }
+            }
+        }
+    }
+
+    public static void main (String [] args){
+        int [] lista = {9,8,7,6,5,4,3,2,1};
+
+        insertion(lista);
+        for ( int i : lista){
+            System.out.print(i + ", ");
+        }
+    }
 }
